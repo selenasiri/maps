58cdaf9fcaa66a34d28e23b98dd722b7ac039187
Selena Siri <selenasiri@gmail.com>
2020-12-28 11:14:35 -0500

    map

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,15 @@ console.log(user);
 const company = new Company();
 console.log(company)
 
-// run by parcel index.html
+// Map
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0  // The center of the map will be at (0, 0)
+  }
+}); // creates an instance of a Map class
+
+
+
+// run by parcel index.html  
